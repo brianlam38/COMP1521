88d9ee0703b43454dc841e75c51a807ea16727d5
brianlam38 <brian.lam38@gmail.com>
2017-07-30 22:10:50 +1000

    adding lab1

---
+++ labs/lab01/Queue.c
@@ -0,0 +1,110 @@
+// ADT for a FIFO queue
+// COMP1521 17s2 Week01 Lab Exercise
+// Written by John Shepherd, July 2017
+// Modified by ...
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include "Queue.h"
+
+typedef struct QueueNode {
+   int jobid;  // unique job ID
+   int size;   // size/duration of job
+   struct QueueNode *next;
+} QueueNode;
+
+struct QueueRep {
+   int nitems;      // # of nodes
+   QueueNode *head; // first node
+   QueueNode *tail; // last node
+};
+
+#if 0
+
+// TODO:
+// remove the #if 0 and #endif
+// once you've added code to use this function
+
+// create a new node for a Queue
+static
+QueueNode *makeQueueNode(int id, int size)
+{
+   QueueNode *new;
+   new = malloc(sizeof(struct QueueNode));
+   assert(new != NULL);
+   new->jobid = id;
+   new->size = size;
+   new->next = NULL;
+   return new;
+}
+
+#endif
+
+
+// make a new empty Queue
+Queue makeQueue()
+{
+   Queue new;
+   new = malloc(sizeof(struct QueueRep));
+   assert(new != NULL);
+   new->nitems = 0; new->head = new->tail = NULL;
+   return new;
+}
+
+// release space used by Queue
+void  freeQueue(Queue q)
+{
+   assert(q != NULL);
+   // TODO
+}
+
+// add a new item to tail of Queue
+void  enterQueue(Queue q, int id, int size)
+{
+   assert(q != NULL);
+   // TODO
+}
+
+// remove item on head of Queue
+int   leaveQueue(Queue q)
+{
+   assert(q != NULL);
+   // TODO
+   return 0; // replace this statement
+}
+
+// count # items in Queue
+int   lengthQueue(Queue q)
+{
+   assert(q != NULL);
+   return q->nitems;
+}
+
+// return total size in all Queue items
+int   volumeQueue(Queue q)
+{
+   assert(q != NULL);
+   // TODO
+   return 0; // replace this statement
+}
+
+// return size/duration of first job in Queue
+int   nextDurationQueue(Queue q)
+{
+   assert(q != NULL);
+   // TODO
+   return 0; // replace this statement
+}
+
+
+// display jobid's in Queue
+void showQueue(Queue q)
+{
+   QueueNode *curr;
+   curr = q->head;
+   while (curr != NULL) {
+      printf(" (%d,%d)", curr->jobid, curr->size);
+      curr = curr->next;
+   }
+}
