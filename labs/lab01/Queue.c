8eb2f939a0c15f94c744a13e85e60f623380757a
brianlam38 <brian.lam38@gmail.com>
2017-07-31 18:57:51 +1000

    fixing queue ADT implementation

--- labs/lab01/Queue.c
+++ labs/lab01/Queue.c
@@ -46,39 +46,37 @@ Queue makeQueue()
 // release space used by Queue
 void  freeQueue(Queue q)
 {
-   printf("=== FREEING QUEUE ===\n");
    assert(q != NULL);
 
    QueueNode *curr = q->head;    // curr ptr
    while (curr != NULL) {        // while not @end
       QueueNode *prev = curr;
       curr = curr->next;         // curr = next
-      free(prev);                // remove prev  
-      assert(prev == NULL);           
+      free(prev);                // remove prev
    }
    free(q);
-   assert(q == NULL); 
 }
 
 // add a new item to tail of Queue
 void  enterQueue(Queue q, int id, int size)
 {
-   printf("=== ADDING NEW NODE TO Q ===\n");
+   //printf("=== ADDING NEW NODE TO Q ===\n");
    assert(q != NULL);
 
-   // TODO
    QueueNode *new = makeQueueNode(id, size);
-   QueueNode *curr = q->head;
 
-   printf("begin loop\n");
-   while(curr != NULL) {
-      curr = curr->next;
+   if (q->head == NULL) {
+      //printf("q->head = NULL\n");
+      q->head = new;
+      q->tail = new;
+      q->nitems++;
+   } else {
+      //printf("q->head != NULL\n");
+      QueueNode *old = q->tail;
+      old->next = new;
+      q->tail = new;
+      q->nitems++;
    }
-   curr = new;
-
-   printf("linking tail to new\n");
-   q->tail = new;
-   q->nitems++;
 }
 
 // remove item on head of Queue
@@ -86,30 +84,29 @@ int   leaveQueue(Queue q)
 {
    assert(q != NULL);
 
-   printf("=== REMOVING NODE FROM Q HEAD ===\n");
-   // TODO
+   //printf("=== REMOVING NODE FROM Q HEAD ===\n");
+   // head is null
+   if (q->head == NULL) return 0;
+
    int id = 0;
 
-   QueueNode *curr = q->head;
-   QueueNode *old = NULL;
-
-   printf("part 1\n");
-   if (q->head != NULL) {
-      printf("part 2\n");
-      curr = curr->next;
-      q->head = curr;
-      old = q->head;
-      id = old->jobid;
-      free(old);
-   } else {
-      printf("part 3\n");
-      id = curr->jobid;
-      printf("part 4\n");
-      free(curr);
+   // next node is null
+   if (q->head->next == NULL) {
+      id = q->head->jobid;
+      q->nitems--;
+      free(q->head);
+      return id;
    }
 
+   // next node exists, move curr to next, return old head ID
+   QueueNode *curr = q->head;
+   QueueNode *old = q->head;
+   curr = curr->next;
+   q->head = curr;
+   id = old->jobid;
    q->nitems--;
 
+   free(old);
    return id;
 }
 
@@ -124,25 +121,25 @@ int   lengthQueue(Queue q)
 int   volumeQueue(Queue q)
 {
    assert(q != NULL);
-   printf("=== COUNTING Q VOLUME ===\n");
-   // TODO
-   QueueNode *curr = q->head;    // curr ptr
+
+   //printf("=== COUNTING Q VOLUME ===\n");
+   if (q->head == NULL) return 0;
+
    int volume = 0;
-   while (curr != NULL) {        // while not @end
-      printf("summing node size\n");
+   QueueNode *curr = q->head;
+   while (curr != NULL) { 
       volume += curr->size;
       curr = curr->next;          
    }
-   printf("TOTAL VOLUME = %d \n", volume);
-   return volume; // replace this statement
+   return volume;
 }
 
 // return size/duration of first job in Queue
 int   nextDurationQueue(Queue q)
 {
    assert(q != NULL);
-   printf("=== COUNTING SIZE OF Q HEAD ===\n");
-   // TODO
+
+   //printf("=== COUNTING SIZE OF Q HEAD ===\n");
    if (q->head != NULL) {
       return q->head->size;
    }
