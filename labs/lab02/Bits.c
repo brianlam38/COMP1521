fb8df8b209680b8cc2cb1aa83416773c71b57fae
brianlam38 <brian.lam38@gmail.com>
2017-08-13 14:45:34 +1000

    updating bitops program

--- labs/lab02/Bits.c
+++ labs/lab02/Bits.c
@@ -126,23 +126,34 @@ void setBitsFromString(Bits b, char *bitseq)
    /* PRINT TEST */
    int i = 0;
    while (bitseq[i] != '\0') {
-      printf("char = %c\n", bitseq[i]);
+      printf("bit = %u\n", bitseq[i]);
       i++;
    }
+
+   // count size of bits in words array
+   // count size of bits in bit-string
+   // find diff in bits between two
+   // set value = 0 for each index until up to diff
+
+   int arrayLen = b->nwords*BITS_PER_WORD;
+   int strLen = strlen(bitseq);
+   int diff = arrayLen - strLen;
 
    // read in the bit-string
    // for each bit-string, separate length by 32 = nwords
    // starting from words[i] until words[n], add char to b->words[i]
-
+   /*
    for (int i = 0; i < b->nwords; i++) {
       for (int j = 0; j < BITS_PER_WORD; j++) {
          unsigned int x = 0;
          if (bitseq[i] == '1') x = 1;
+         if (bitseq[i] == '0') x = 0;
+
          b->words[j] = x;
-         printf("%c", bitseq[i]);
          printf("%u", b->words[j]);
       }
    }
+   */
 }
 
 // display a Bits value as sequence of 0's and 1's
