508be2b19111f7a4d915b99657358abfecd9b481
brianlam38 <brian.lam38@gmail.com>
2017-08-21 14:49:47 +1000

    adding w1 notes

---
+++ notes/w1.c
@@ -0,0 +1,14 @@
+/* WEEK 1 NOTES
+ *
+ * Brian Lam
+ */
+
+// C is a step up from assembly language.
+Stack: Last In First Out
+- push(), pop(), top, size
+- used in implementation of functions compiled in C
+
+Queue: First In First Out
+- enqueue() dequeue(), head, tail, size
+- used in scheduling, managing resource usage
+
