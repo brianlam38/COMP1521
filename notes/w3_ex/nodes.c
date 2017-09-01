97a48b6b768ba0fa93e5a6d812c24847b9ff7da7
brianlam38 <brian.lam38@gmail.com>
2017-09-02 00:43:53 +1000

    finalise two diff approaches to variable structs

--- notes/w3_ex/nodes.c
+++ notes/w3_ex/nodes.c
@@ -2,14 +2,14 @@
 typedef struct _node1 Node1;
 struct _node1 {
 	char *str;		// pointer to string
-	Node *next;
+	Node1 *next;
 }
 
 // String is defined inside the struct
 typedef struct _node2 Node2;
 struct _node2 {
 	char str[5];	// string defined inside struct
-	Node *next;
+	Node2 *next;
 }
 
 // create new node with its own copy of string
@@ -17,11 +17,27 @@ Node1 *makeNode1(char *s)
 {
 	Node *new;
 	new = malloc(sizeof(Node1));
-	new->string = strdup(s);	// strdup() = makes a copy of string passed in, so that
-	new->next = NULL;			//			  when the function somewhere that passes in
-}								//			  *s stops, it won't destroy data in the struct.
+	//new->string = strdup(s);			METHOD 1: strdup() = makes a copy of string passed in, so that
+	//   											 		when the function somewhere that passes in
+	//														*s stops, it won't destroy data in the struct.
+
+	char *news = malloc(strlen(s)+1);  	 // METHOD 2: malloc size of string + 1 for null byte
+	for (int i = 0; s[i] != '\0'; i++) { //			  copy each char across
+		news[i] = s[i];					 //			  set last char = null byte
+	}
+	news[i] = '\0';
+	new->next = NULL;	
+	return new;		 
+}								
 
 // create new node with its own copy of string
-Node2 *makeNode2(char *str)
+Node2 *makeNode2(char *s)
 {
+	Node2 *new;
+	new = malloc(sizeof(Node2));
+	//strlcpy(new->s, s);			METHOD 1: strlcpy null terminates the copied string automatically
+	strcpy(new->str, s, 4);		 // METHOD 2: strcpy + define size of string + manually add null-byte
+	s[4] = '\0';
+	new->next  = NULL;
+	return new;
 }
