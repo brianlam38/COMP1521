8a2397e3b9702b26b94ff851869ff81325bd1436
brianlam38 <brian.lam38@gmail.com>
2017-09-05 14:06:02 +1000

    updating program comments

--- ass/GameOfLife/life.c
+++ ass/GameOfLife/life.c
@@ -10,27 +10,27 @@ int main(void)
 {
     int maxiters;
     printf("# Iterations: ");
-    scanf("%d", &maxiters);
-    for (int n = 1; n <= maxiters; n++) {
-      for (int i = 0; i < N; i++) {
-         for (int j = 0; j < N; j++) {
-            int nn = neighbours(i,j);
-            if (board[i][j] == 1) {
-               if (nn < 2)
-                  newboard[i][j] = 0;
-               else if (nn ==2 || nn == 3)
-                  newboard[i][j] = 1;
-               else
-                  newboard[i][j] = 0;
-            }
-            else if (nn == 3)
-               newboard[i][j] = 1;
-            else
-               newboard[i][j] = 0;
+    scanf("%d", &maxiters);                   // input number of iterations
+    for (int n = 1; n <= maxiters; n++) {     // for each iteration
+      for (int i = 0; i < N; i++) {           //      for each col
+         for (int j = 0; j < N; j++) {        //          for each row
+            int nn = neighbours(i,j);         //            [LIVING CELL]
+            if (board[i][j] == 1) {           //            if curr cell = 1
+               if (nn < 2)                    //                if neighbours < 2  (underpop: cells with nn < 2 dies)
+                  newboard[i][j] = 0;         //                    updated = 0
+               else if (nn ==2 || nn == 3)    //                else if neighbours = 2 or 3 (lives: cells nn = 2 | 3)
+                  newboard[i][j] = 1;         //                    updated = 1
+               else                           //                else                (overpop: cells with nn > 3)
+                  newboard[i][j] = 0;         //                    updated = 0
+            }                                 //            [DEAD CELL]
+            else if (nn == 3)                 //            else if curr neighbours = 3  (reproduction)
+               newboard[i][j] = 1;            //                updated = 1
+            else                              //            else                    (no reproduction)
+               newboard[i][j] = 0;            //                updated = 0
          }
       }
-      printf("=== After iteration %d ===\n", n);
-      copyBackAndShow();
+      printf("=== After iteration %d ===\n", n);  // print iteration #
+      copyBackAndShow();                          // show updated board for current iteration
    }
    return 0;
 }
