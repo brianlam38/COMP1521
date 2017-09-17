ae705a79c26fe4c197015eb0eae7668616afca1f
brianlam38 <brian.lam38@gmail.com>
2017-09-18 00:55:16 +1000

    updating myls

--- labs/lab06/myls.c
+++ labs/lab06/myls.c
@@ -23,9 +23,9 @@ int main(int argc, char *argv[])
 {
    // string buffers for various names
    char dirname[MAXDIRNAME];
-   // char uname[MAXNAME+1]; // UNCOMMENT this line
-   // char gname[MAXNAME+1]; // UNCOMMENT this line
-   // char mode[MAXNAME+1]; // UNCOMMENT this line
+   char uname[MAXNAME+1]; // UNCOMMENT this line
+   char gname[MAXNAME+1]; // UNCOMMENT this line
+   char mode[MAXNAME+1]; // UNCOMMENT this line
 
    // collect the directory name, with "." as default
    if (argc < 2)
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
 
    // check that the name really is a directory
    struct stat info;
-   if (stat(dirname, &info) < 0)
+   if (lstat(dirname, &info) < 0)
       { perror(argv[0]); exit(EXIT_FAILURE); }
    if ((info.st_mode & S_IFMT) != S_IFDIR)
       { fprintf(stderr, "%s: Not a directory\n",argv[0]); exit(EXIT_FAILURE); }
@@ -48,7 +48,23 @@ int main(int argc, char *argv[])
    // read directory entries
    struct dirent *entry;                        // 1. Declare a Dirent struct, a special struct used to return info about dir entries
    while ((entry = readdir(df)) != NULL) {      // 2. readdir() returns a ptr to the next dir entry.
-      printf("[%s]\n", entry->d_name);          //    returns NULL at the end of the directory
+                                                //    returns NULL at the end of the directory
+      // if object is '.' skip
+      if (strcmp(entry->d_name,".") == 0) continue;
+      lstat(entry->d_name, &info);
+
+      rwxmode(info.st_mode, mode);
+      printf("MODE = %u\n", info.st_mode);         // grab mode / type
+
+      username(info.st_uid, uname);                // grab name of owner
+      printf("UNAME = [%s]\n", uname);
+
+      groupname(info.st_gid, gname);               // grab name of group
+      printf("GNAME = [%s]\n", gname);
+
+      printf("SIZE = [%lld]\n", info.st_size);     // grab file size
+      printf("OBJ NAME = [%s]\n", entry->d_name);  // grab name of file
+      printf("\n");
    }                                            // 3. Loo through each entry and print struct value.
                                                 //    d_name[] is a struct component: a null-terminated file name
 
@@ -62,8 +78,29 @@ int main(int argc, char *argv[])
 // convert octal mode to -rwxrwxrwx string
 char *rwxmode(mode_t mode, char *str)
 {
-   return NULL;
-   // ... TODO ...
+    printf("MODE = %hu", mode);
+    return NULL;
+    if (mode == S_IFDIR) {
+        printf("directory\n");
+    } else if (mode == S_IFREG) {
+        printf("regular file\n");
+    } else if (mode == S_IFLNK) {
+        printf("symbolic link\n");
+    } else {
+        printf("OTHER\n");
+    }
+
+    // print correct filename + read/write privileges
+
+     //#define S_IFMT 0170000           /* type of file */
+     //#define        S_IFIFO  0010000  /* named pipe (fifo) */
+     //#define        S_IFCHR  0020000  /* character special */
+     //#define        S_IFDIR  0040000  /* directory */
+     //#define        S_IFBLK  0060000  /* block special */
+     //#define        S_IFREG  0100000  /* regular */
+     //#define        S_IFLNK  0120000  /* symbolic link */
+     //#define        S_IFSOCK 0140000  /* socket */
+     //#define        S_IFWHT  0160000  /* whiteout */
 }
 
 // convert user id to user name
