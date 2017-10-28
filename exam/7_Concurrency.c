17c2e2e86200ffb442afafcda6b53eaced3113a7
brianlam38 <brian.lam38@gmail.com>
2017-10-28 13:01:46 +1100

    adding flock notes

--- exam/7_Concurrency.c
+++ exam/7_Concurrency.c
@@ -5,7 +5,7 @@ Proceses can interact with each other in several ways.
 'Non-deterministic' (no two is the same) problems can arise from un-controlled interactions.
 Mechanisms exist to control interactions / access to files e.g. flock()
 
-'FILE LOCKING' flock(int FileDesc, int Operation)
+FILE LOCKING: flock(int FileDesc, int Operation)
 -> Controls access to shared files
 -> Operations:
 	'LOCK_SH' ... acquire shared lock
@@ -15,3 +15,13 @@ Mechanisms exist to control interactions / access to files e.g. flock()
 -> flock() only ATTEMPTS to acquire a lock, only works correctly if all processes that access the file use locks
 -> return 0 success | return -1 failure
 
+FILE LOCKING SCENARIOS:
+1. Process tries to acquire a 'shared lock - LOCK_SH':
+   -> If file NOT locked or other shared locks = OK
+   -> If file locked = BLOCKED
+2. Process tries to acquire an 'exclusive lock - LOCK_EX'
+   -> If file NOT lcoked = OK
+   -> If file shared or exclusive lock = BLOCKED
+3. Process using a 'non-exclusive lock'
+   -> return 0  if locked acquired
+   -> return -1 if procecess would be bocked
