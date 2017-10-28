122f9dce71eb2ffbbc25a62b0406fb91c99743e6
brianlam38 <brian.lam38@gmail.com>
2017-10-28 12:55:49 +1100

    adding flock() program

--- exam/7_Concurrency.c
+++ exam/7_Concurrency.c
@@ -1,9 +1,9 @@
 // CONCURRENCY
 
-====== 'INTERACTING PROCESSES' ======
+====== 'INTERACTING PROCESSES / CONTROLLING ACCESS via. FLOCK()' ======
 Proceses can interact with each other in several ways.
 'Non-deterministic' (no two is the same) problems can arise from un-controlled interactions.
-Mechanisms exist to control interactions e.g. flock()
+Mechanisms exist to control interactions / access to files e.g. flock()
 
 'FILE LOCKING' flock(int FileDesc, int Operation)
 -> Controls access to shared files
@@ -14,3 +14,4 @@ Mechanisms exist to control interactions e.g. flock()
 	'LOCK_NB' ... operation fails rather than blocking
 -> flock() only ATTEMPTS to acquire a lock, only works correctly if all processes that access the file use locks
 -> return 0 success | return -1 failure
+
